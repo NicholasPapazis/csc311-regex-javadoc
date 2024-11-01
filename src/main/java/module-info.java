4b07cb9ab433e54fceeebfe9e20a95e297d75127
Nicholas Papazis <nicholasepapazis@gmail.com>
2024-11-01 12:44:44 -0400

    updated gui

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.regexjavadoccsc311 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.example.regexjavadoccsc311 to javafx.fxml;
--- src/main/java/org/example/regexjavadoccsc311/LoginViewController.java
+++ src/main/java/org/example/regexjavadoccsc311/LoginViewController.java
@@ -1,4 +1,25 @@
 package org.example.regexjavadoccsc311;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+import java.awt.event.ActionEvent;
+
 public class LoginViewController {
+
+    @FXML
+    private TextField firstNameInput;
+    @FXML
+    private Pane inputWrapper;
+
+
+    public void firstNameInput(MouseEvent mouseEvent) {
+        inputWrapper.setStyle("-fx-border-color: #3B8EDC"); //add border when clicked
+    }
+
+    public void focusLostFirstName(MouseEvent mouseEvent) {
+        inputWrapper.setStyle("-fx-border-color: transparent;"); //remove border when focus is lost
+    }
 }
